504a018583e0451c42334129c74305cae09c3a25
gayathri6161 <gayathrichowdarykoya9182@gmail.com>
2026-01-22 10:21:49 +0530

    22-01-2026

---
+++ DI-EcommerceApp/src/main/java/com/klu/Configuration/AppConfig.java
@@ -0,0 +1,10 @@
+package com.klu.Configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages="com.klu.Model")
+public class AppConfig {
+
+}
